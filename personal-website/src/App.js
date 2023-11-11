4e7b9739ff42a97fe9bedd6667b7f41dfa4e0fc9
Faraz shafi <farazpachu777@gmail.com>
2023-11-11 08:01:27 +0530

    Created a new personal webstie project

---
+++ personal-website/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Faraz Shafi</h3>
+      <p>Full stack developer</p>
+    </div>
+  );
+}
+
+export default App;
